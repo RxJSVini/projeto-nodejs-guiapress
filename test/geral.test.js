6588587af2a1439d2dc515aa7e95647ff6ad8c9b
Vinicius  Pereira <viniciuspoa2@hotmail.com>
2021-05-31 18:34:05 -0300

    Delete test directory

--- test/geral.test.js
+++
@@ -1,8 +0,0 @@
-const supertest = require('supertest');
-const app = require('../app');
-const request = supertest(app);
-
-it("Shound something", async () => {
-  const resp = await request.get("/")
-  expect(resp.status).toEqual(200)
-});
