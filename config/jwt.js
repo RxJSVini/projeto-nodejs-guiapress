f304cf5b59989e66d57b5dbfbec9cf5222cdd8d1
Vinicius  Pereira <viniciuspoa2@hotmail.com>
2021-05-31 18:33:55 -0300

    Delete config directory

--- config/jwt.js
+++
@@ -1,6 +0,0 @@
-const jwt = require('jsonwebtoken');
-
-module.exports = {
-  secret: 'gu1@pr3$3312c5f7f44ec6091d9dc576',
-  expiresIn: '10d'
-}
--- config/testeJWT.js
+++
@@ -1,4 +0,0 @@
-const configJwt = require('./jwt')
-const jwt = require('jsonwebtoken');
-
-console.log(jwt.verify('adasdasdasdasdasdasd', configJwt.secret));
